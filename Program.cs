03aee2142ac9c7ff0f2ca4e2d744982c367f940e
Hristo <hristo.anastasov@gmail.com>
2022-08-09 14:01:57 +0300

    untitled-app (5) initial version

---
+++ Program.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using IgniteUI.Blazor.Controls;
+using UntitledApp5;
+
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+RegisterIgniteUI(builder.Services);
+
+await builder.Build().RunAsync();
+
+void RegisterIgniteUI(IServiceCollection services)
+{
+    services.AddIgniteUIBlazor(
+        typeof(IgniteUI.Blazor.Controls.IgbTreeModule)
+    );
+}
